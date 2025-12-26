cd67c26c6677bdcaf09ec56eb1dc31eb665ce739
muzi5@kakao.com <muzi5@kakao.com>
2025-12-27 02:28:16 +0900

    feat: 소셜 로그인 기능 추가 및 콜백 URL 설정

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,20 @@ import { db } from "@/app/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/app/db/schema";
+
 export const auth = betterAuth({
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env
+        .GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env
+        .GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
