af1571fbe6f79047100a027747d45908fa5264bc
muzi5@kakao.com <muzi5@kakao.com>
2025-12-28 23:49:08 +0900

    feat: 모바일 사용자 버튼에 드로어 메뉴 추가

--- app/modules/dashboard/ul/components/dashboard-user-button.tsx
+++ app/modules/dashboard/ul/components/dashboard-user-button.tsx
@@ -6,6 +6,8 @@ import {
   AvatarImage,
 } from "@/components/ui/avatar";
 import {
+  DropdownMenu,
+  DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -14,19 +16,29 @@ import {
 import { authClient } from "@/lib/auth-client";
 
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 import {
   ChevronDownIcon,
   CreditCardIcon,
   LogOutIcon,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export default function DashboardUserButton() {
   const router = useRouter();
   const { data, isPending } = authClient.useSession();
+
+  const isMobile = useIsMobile();
 
   const onLogout = () => {
     authClient.signOut({
@@ -38,6 +50,67 @@ export default function DashboardUserButton() {
   if (!data?.user || isPending) {
     return null;
   }
+
+  console.log("isMobile", isMobile);
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger
+          className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-black overflow-hidden"
+          aria-label="유저 메뉴 열기">
+          {data.user.image ? (
+            <Avatar>
+              <AvatarImage
+                src={data.user.image}
+                alt={data.user.name ?? "User Avatar"}
+              />
+            </Avatar>
+          ) : (
+            <GeneratedAvatar
+              seed={data.user.name ?? "User"}
+              variant="initials"
+              className="size-9 mr-3"
+            />
+          )}
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+            <p className="text-sm truncate w-full">
+              {data.user.name}
+            </p>
+            <p className="text-xs truncate w-full">
+              {data.user.email}
+            </p>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0 " />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerDescription>
+              {data.user.email}
+            </DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => {}}>
+              <CreditCardIcon className="size-4 text-black4" />
+              Billing
+            </Button>
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={onLogout}>
+              <LogOutIcon className="size-4 text-black4" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-black overflow-hidden">
