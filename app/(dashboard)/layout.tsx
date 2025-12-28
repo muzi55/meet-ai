886b40b7a302f522e9a449299184b86382109d44
muzi5@kakao.com <muzi5@kakao.com>
2025-12-28 23:37:11 +0900

    feat: 대시보드 레이아웃에 내비게이션 바 추가 및 명령어 대화상자 구현

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 import { DashboardSidebar } from "../modules/dashboard/ul/components/dashboard-sidebar";
+import DashboardNavbar from "../modules/dashboard/ul/components/dashboard-navbar";
 
 export default function DashBoardLayout({
   children,
@@ -11,6 +12,7 @@ export default function DashBoardLayout({
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
