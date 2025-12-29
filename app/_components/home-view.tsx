6bef80c9b5981fb2a10c170d9784222572feb9ce
muzi5@kakao.com <muzi5@kakao.com>
2025-12-29 23:51:35 +0900

    feat: TRPC 및 React Query 통합 구현

--- app/_components/home-view.tsx
+++ app/_components/home-view.tsx
@@ -1,29 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "../trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export function HomeView() {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-
-  if (!session) {
-    return <p>로그인 해주세요</p>;
-  }
-  return (
-    <div>
-      <p>환영합니다, {session.user.name}!</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => router.push("/sign-in"),
-            },
-          })
-        }>
-        Sign out
-      </Button>
-    </div>
+  const trpc = useTRPC();
+  const { data } = useQuery(
+    trpc.hello.queryOptions({
+      text: "from tRPC",
+    })
   );
+
+  return <div>{data?.greeting}</div>;
 }
