3bf9010b58765165f56e67b6a4205a352df14ae1
muzi5@kakao.com <muzi5@kakao.com>
2025-12-29 23:58:56 +0900

    style: TRPCReactProvider 위치 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-      <html lang="ko">
-        <body className={`${inter.className} antialiased`}>
-          {children}
-        </body>
-      </html>
-    </TRPCReactProvider>
+    <html lang="ko">
+      <body className={`${inter.className} antialiased`}>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
   );
 }
